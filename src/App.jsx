0303be3fe93579b3fa54da5aa4c4949efbbdd4f5
j4web <prakharsrivastava841@gmail.com>
2023-07-26 23:20:01 +0530

    �

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="parent">
+    <div className="player">
+      <div className="legs"></div>
+      <div className="back"></div>
+      <div className="glass"></div>
+      <div className="shadow"></div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
